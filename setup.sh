c70a108ffcb27eb7055e130bdd4226e2b8530e12
AI-20-CO <73228148+AI-20-CO@users.noreply.github.com>
2022-08-15 22:54:48 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+[theme]
+primaryColor='#85878a'
+backgroundColor='#000000'
+secondaryBackgroundColor='#212123'
+textColor='#454848'
+" > ~/.streamlit/config.toml
